65a44960a6a0c4484c2a88c050708f4c1171d3a3
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-10-09 13:53:39 +0000

    a

--- PP2_1.py
+++ PP2_1.py
@@ -2,10 +2,20 @@
 
 def q1(): 
   #Write Assignment code here
+  a = int(input("Input a integer:"))
 
+  if a % 2 == 0:
+    print(f"{a} is even")
+  else:
+    print(f"{a} is odd")
 
 def q2(): 
   #Write Assignment code here
+  b = input("Input a name:")
+  if b == "Kalisz":
+    print("teacher")
+  else :
+    print("student")
 
 
 
